76ca586248175bf77c154eb4f25fb7597c2de499
devhdhawan <127012062+devhdhawan@users.noreply.github.com>
2025-07-11 17:11:49 +0530

    Time: 46 ms (17.01%) | Memory: 45.1 MB (14.96%) - LeetSync

--- DSA/leetcode/cpp/39-combination-sum/combination-sum.cpp
+++ DSA/leetcode/cpp/39-combination-sum/combination-sum.cpp
@@ -2,32 +2,24 @@ class Solution {
 public:
     vector<vector<int>> ans;
 
-    void sum(vector<int>& candidates,int target,int index,int size,vector<int> tmp){
-                if(target==0){
-                    ans.push_back(tmp);
-                    return ;
-                }
-                if(index>=size)
-                    return;
-                
-                if(target<0)
-                    return ;
-                
-                int len=target/candidates[index];
-                vector<int> tmp2=tmp;
-                int sum2=0;
-                for(int i=0;i<len;i++){
-                    tmp2.push_back(candidates[index]);
-                    sum2=sum2+candidates[index];
-                    sum(candidates,target-sum2,index+1,size,tmp2);
-                }
-                
-               
-                sum(candidates,target,index+1,size,tmp);
+    void findCombinations(vector<int>& candidates,int target,int index,int size,vector<int> tmp){
+        if(index==size){
+            if(target==0)
+                ans.push_back(tmp);
+            return;
+        }
+
+        if(candidates[index]<=target){
+            tmp.push_back(candidates[index]);
+            findCombinations(candidates,target-candidates[index],index,size,tmp);
+            tmp.pop_back();
+        }
+
+        findCombinations(candidates,target,index+1,size,tmp);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> tmp;
-        sum(candidates,target,0,candidates.size(),tmp);
+        findCombinations(candidates,target,0,candidates.size(),tmp);
 
         return ans;
     }
