c61b9abaf93bdfcaa30d98129a93511cf1d1558a
devhdhawan <127012062+devhdhawan@users.noreply.github.com>
2025-07-11 16:03:51 +0530

    Time: 2 ms (44.94%) | Memory: 11.4 MB (24.44%) - LeetSync

--- DSA/leetcode/cpp/90-subsets-ii/subsets-ii.cpp
+++ DSA/leetcode/cpp/90-subsets-ii/subsets-ii.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> ans;
+
     void powerSet(vector<int>& nums,vector<int> tmp,int index,int size){
-            if(index>=size)
-                return;
-            
-           
-            powerSet(nums,tmp,index+1,size);
-
-           
-            tmp.push_back(nums[index]);
-
-            powerSet(nums,tmp,index+1,size);
-
-             ans.push_back(tmp);
+        ans.push_back(tmp);
+        
+        for(int i=index;i<size;i++){
+            if(i!=index and nums[i]==nums[i-1])
+                continue;
+            tmp.push_back(nums[i]);
+            powerSet(nums,tmp,i+1,size);
+            tmp.pop_back();
+        }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<int> tmp;
-        ans.push_back(tmp);
         sort(nums.begin(),nums.end());
+        vector<int> tmp;
         powerSet(nums,tmp,0,nums.size());
 
-        sort(ans.begin(),ans.end());
-
-        for(int i=1;i<ans.size();i++){
-            while(ans[i]==ans[i-1]){
-                ans.erase(ans.begin()+i);   
-
-            }
-        }
         return ans;
-
-
     }
 };
