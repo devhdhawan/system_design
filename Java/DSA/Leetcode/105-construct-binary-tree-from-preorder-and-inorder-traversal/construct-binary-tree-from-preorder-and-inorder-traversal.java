e2c7d05cdf91f1d994984e55e1e779c5ec11419b
devhdhawan <127012062+devhdhawan@users.noreply.github.com>
2025-07-31 19:18:29 +0530

    Time: 2 ms (60.54%) | Memory: 44.8 MB (19.60%) - LeetSync

---
+++ Java/DSA/Leetcode/105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer,Integer> mp=new HashMap<>();
+    public TreeNode solve(TreeNode root,int[] preorder,Solution obj,int preStart,int preEnd,int inStart,int inEnd){
+        
+            if(inStart>inEnd)
+                return null;
+            if(preStart>preEnd)
+                return null;
+            
+            int inIndex=obj.mp.get(preorder[preStart]);
+            int diff=inIndex-inStart;
+            int preIndex=preStart+diff;
+
+            root=new TreeNode(preorder[preStart]);
+          
+            root.left=solve(root.left,preorder,obj,preStart+1,preIndex,inStart,inIndex-1);
+            root.right=solve(root.right,preorder,obj,preIndex+1,preEnd,inIndex+1,inEnd);
+
+            return root;
+    }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Solution obj=new Solution();
+
+        for(int i=0;i<inorder.length;i++){
+            obj.mp.put(inorder[i],i);
+        }
+
+        // for(int i=0;i<inorder.length;i++){
+        //     System.out.println(inorder[i]+" "+obj.mp.get(inorder[i]));
+        // }
+        TreeNode root=new TreeNode();
+        return solve(root,preorder,obj,0,preorder.length-1,0,inorder.length-1);
+
+        
+    }
+}
